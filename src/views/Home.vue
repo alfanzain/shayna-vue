bd8354c3dc01dcc36b7e79ad523ec41b09d4c1ac
Alfan Zain <alfanzainkp@gmail.com>
2020-10-05 08:57:59 +0700

    Initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="home">
+    <!-- Header Section Begin -->
+    <Header />
+    <!-- Header End -->
+
+    <!-- Hero Section Begin -->
+    <Hero />
+    <!-- Hero Section End -->
+
+    <!-- Women Banner Section Begin -->
+    <Banner />
+    <!-- Women Banner Section End -->
+
+    <!-- Partner Logo Section Begin -->
+    <Partner />
+    <!-- Partner Logo Section End -->
+
+    <!-- Footer Section Begin -->
+    <Footer />
+    <!-- Footer Section End -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import Header from '@/components/Header.vue'
+import Hero from '@/components/Hero.vue'
+import Banner from '@/components/Banner.vue'
+import Partner from '@/components/Partner.vue'
+import Footer from '@/components/Footer.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Header,
+    Hero,
+    Banner,
+    Partner,
+    Footer
+  }
+}
+</script>
